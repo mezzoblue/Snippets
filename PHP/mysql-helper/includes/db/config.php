2c00f9157a6958ba09030c7ce4eae2aec8938df7
Dave Shea <dave@mezzoblue.com>
2011-04-11 23:25:06 -0700

    Adding mysql-helper

---
+++ PHP/mysql-helper/includes/db/config.php
@@ -0,0 +1,1 @@
+<?	// all database configuration settings here	$config = array(		"host" => array(			"name" => "localhost",			"user" => "root",			"password" => "root",		),		"db" => array(			"name" => "testing",		),	);?>
---
+++ PHP/mysql-helper/includes/diagnostics/environment.php
@@ -0,0 +1,1 @@
+<?	// get some information about the server environment	echo "Document root: " . $_SERVER['DOCUMENT_ROOT'] . "<br>";	echo "Filename: " . $_SERVER['PHP_SELF'] . "<br>";	echo "Absolute pathname: " . $_SERVER['SCRIPT_FILENAME'] . "<br>";	echo "Script name: " . $_SERVER['SCRIPT_NAME'] . "<br>";?>

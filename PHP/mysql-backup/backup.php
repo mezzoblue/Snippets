39140ba03fdaf181789ba729505e9bf0c54a1f84
Dave Shea <dave@mezzoblue.com>
2011-04-17 18:34:10 -0700

    Moving separate configs out to common include in mysql-backup

--- PHP/mysql-backup/backup.php
+++ PHP/mysql-backup/backup.php
@@ -2,22 +2,8 @@
 
 	// MySQL Backup Tool
 
-	// all database configuration settings here
-	$config = array(
-
-		// host defines database host, plus username and password
-		"host" => array(
-			"name" => "localhost",
-			"user" => "root",
-			"password" => "root",
-		),
-
-		// db defines name of the database you'd like to work with, plus an optional filename for importing/exporting
-		"db" => array(
-			"name" => "testing",
-			"backupFile" => "db-backup-" . date("Y-m-d-(H-i-s)") . ".sql",
-		),
-	);
+	// database server configuration
+	include ("config.php");
 
 	// backup an entire db, or just a table, to a flat file
 	// (assumes open connection)
--- PHP/mysql-backup/restore.php
+++ PHP/mysql-backup/restore.php
@@ -2,23 +2,8 @@
 
 	// MySQL Restore Tool
 
-	// all database configuration settings here
-	$config = array(
-
-		// host defines database host, plus username and password
-		"host" => array(
-			"name" => "localhost",
-			"user" => "root",
-			"password" => "root",
-		),
-
-		// db defines name of the database you'd like to work with, plus an optional filename for importing/exporting
-		"db" => array(
-			"name" => "testing",
-			"backupFile" => "db-backup-" . date("Y-m-d-(H-i-s)") . ".sql",
-		),
-	);
-
+	// database server configuration
+	include ("config.php");
 
 	// create a new database
 	// (assumes open connection)
